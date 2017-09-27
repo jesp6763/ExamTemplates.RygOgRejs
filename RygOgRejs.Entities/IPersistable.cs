da62b062d4239ecd48c31070cf949f8f6c17361f
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-27 13:22:41 +0200

    Added IPersistable interface

---
+++ RygOgRejs.Entities/IPersistable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RygOgRejs.Entities
+{
+    ´public interface IPersistable
+    {
+        /// <summary>
+        /// ID of the entity
+        /// </summary>
+        int Id { get; }
+    }
+}
