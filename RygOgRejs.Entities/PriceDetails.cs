2ca2b6fd42916e55fa3b4afebac5928acc3c33c7
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-27 13:29:41 +0200

    Added PriceDetails class

---
+++ RygOgRejs.Entities/PriceDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RygOgRejs.Entities.Enums;
+
+namespace RygOgRejs.Entities
+{
+    public struct PriceDetails
+    {
+        //private double taxRate;
+
+        /// <summary>
+        /// Initializes a new instance of the PriceDetails class
+        /// </summary>
+        /// <param name="destination">The desired destination</param>
+        /// <param name="adults">How many adults</param>
+        /// <param name="children">How many children</param>
+        /// <param name="isFirstClass">Is it first class</param>
+        /// <param name="extraLuggage">Extra luggage more than 25kg</param>
+        public PriceDetails(Destination destination, int adults, int children, bool isFirstClass, double extraLuggage)
+        {
+            
+        }
+
+        public decimal GetTaxAmount()
+        {
+            return 0;
+        }
+
+        public decimal GetTotalWithoutTax()
+        {
+            return 0;
+        }
+
+        public decimal GetTotalWithTax()
+        {
+            return 0;
+        }
+    }
+}
