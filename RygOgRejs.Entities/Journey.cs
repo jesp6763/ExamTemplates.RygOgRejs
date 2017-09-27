03a3540c2ba50732b4d8273baa163849eebb37a5
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-27 13:30:04 +0200

    Created Journey class. Still work in progress

---
+++ RygOgRejs.Entities/Journey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RygOgRejs.Entities
+{
+    public class Journey
+    {
+        protected int adults;
+        protected int children;
+        protected PriceDetails currentPriceDetails;
+        protected DateTime departureDate;
+        protected bool isFirstClass;
+        protected double luggageAmount;
+
+
+    }
+}
