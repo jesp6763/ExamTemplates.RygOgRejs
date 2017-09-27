defd8710fc0ed2644bf018d8226fe4be957aebfa
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-27 12:01:40 +0200

    Created transaction class

--- RygOgRejs.Entities/Transaction.cs
+++ RygOgRejs.Entities/Transaction.cs
@@ -8,5 +8,34 @@ namespace RygOgRejs.Entities
 {
     public class Transaction
     {
+        /// <summary>
+        /// The paid amount
+        /// </summary>
+        private decimal amount;
+        /// <summary>
+        /// The journey
+        /// </summary>
+        private Journey journey;
+        /// <summary>
+        /// The payer
+        /// </summary>
+        private Payer payer;
+        /// <summary>
+        /// The timestamp of the transaction
+        /// </summary>
+        private DateTime timeStamp;
+
+        /// <summary>
+        /// Initializes a new instance of the Transaction class
+        /// </summary>
+        /// <param name="amount">The paid amount</param>
+        /// <param name="jouney">The journey</param>
+        /// <param name="payer">The payer</param>
+        public Transaction(decimal amount, Journey jouney, Payer payer)
+        {
+            this.amount = amount;
+            this.journey = journey;
+            this.payer = payer;
+        }
     }
 }
