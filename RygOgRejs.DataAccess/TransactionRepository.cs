dbf614e863f5d156eb94e53fd5a7fcb32dd474f0
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-28 09:53:20 +0200

    Created Transaction Repository

---
+++ RygOgRejs.DataAccess/TransactionRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RygOgRejs.DataAccess
+{
+    /// <summary>
+    /// Represents a transaction repository
+    /// </summary>
+    public class TransactionRepository : DataRepository
+    {
+        /// <summary>
+        /// Gets the totals for date
+        /// </summary>
+        /// <param name="date">The date to get transactions from</param>
+        public void GetTotalsFor(DateTime date)
+        {
+            
+        }
+    }
+}
