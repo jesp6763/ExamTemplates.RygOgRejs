9dbf5e5b1da0f56b71581ce35b26ef29102e64bd
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-28 09:37:23 +0200

    Added journey repository

---
+++ RygOgRejs.DataAccess/JourneyRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RygOgRejs.Entities;
+
+namespace RygOgRejs.DataAccess
+{
+    /// <summary>
+    /// Represents a journey repository
+    /// </summary>
+    public class JourneyRepository : DataRepository
+    {
+        /// <summary>
+        /// Gets all journeys
+        /// </summary>
+        /// <returns>A list of journeys</returns>
+        public List<Journey> GetAll()
+        {
+            return new List<Journey>();
+        }
+
+        /// <summary>
+        /// Get a journey by full name
+        /// </summary>
+        /// <param name="payerFullName">The payer's fullname</param>
+        /// <returns>A journey</returns>
+        public Journey GetJourneyBy(string payerFullName)
+        {
+            return null;
+        }
+    }
+}
