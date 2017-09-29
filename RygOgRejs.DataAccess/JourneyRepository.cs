9dae6df30a9a9011795c9803a54f92782029b901
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-29 10:22:50 +0200

    Added extract row method and made use of the new method in the other methods

--- RygOgRejs.DataAccess/JourneyRepository.cs
+++ RygOgRejs.DataAccess/JourneyRepository.cs
@@ -23,24 +23,9 @@ namespace RygOgRejs.DataAccess
             List<Journey> journeyList = new List<Journey>();
             for(int i = 0; i < journeys.Tables[0].Rows.Count; i++)
             {
-                DataRow row = journeys.Tables[0].Rows[i];
-                Destination destination = Destination.Billund;
-                DateTime departureTime = row.Field<DateTime>("DepartureTime");
-                bool isFirstClass = row.Field<bool>("IsFirstClass");
-                int adults = row.Field<int>("Adults"), children = row.Field<int>("Children");
-                double luggageAmount = (double)row.Field<decimal>("LuggageAmount");
+                object[] journeyData = ExtractRow(journeys.Tables[0].Rows[i]);
 
-                switch(row.Field<string>("Destination"))
-                {
-                    case "Copenhagen":
-                        destination = Destination.Copenhagen;
-                        break;
-                    case "PalmaDeMallorca":
-                        destination = Destination.PalmaDeMallorca;
-                        break;
-                }
-
-                Journey journey = new Journey(destination, departureTime, isFirstClass, adults, children, luggageAmount);
+                Journey journey = new Journey((Destination)journeyData[0], (DateTime)journeyData[1], (bool)journeyData[2], (int)journeyData[3], (int)journeyData[4], (double)journeyData[5]);
                 journeyList.Add(journey);
             }
 
@@ -54,9 +39,37 @@ namespace RygOgRejs.DataAccess
         /// <returns>A journey</returns>
         public Journey GetJourneyBy(string payerFullName)
         {
-            DataSet dataSet = executor.Execute($"SELECT * FROM dbo.Payers WHERE Firstname + ' ' + Lastname LIKE '{payerFullName}%'");
+            DataSet dataSet = executor.Execute($"SELECT JourneyId FROM dbo.Payers WHERE Firstname + ' ' + Lastname LIKE '{payerFullName}%'");
             DataRow row = dataSet.Tables[0].Rows[0];
-            return new Journey(row.Field<Destination>("Destination"), row.Field<DateTime>("DepartureTime"), row.Field<bool>("IsFirstClass"), row.Field<int>("Adults"), row.Field<int>("Children"), row.Field<double>("LuggageAmount"));
+            object[] journeyData = ExtractRow(executor.Execute($"SELECT * FROM dbo.Journeys WHERE JourneyId={row.Field<string>("JourneyId")}").Tables[0].Rows[0]);
+
+            return new Journey((Destination)journeyData[0], (DateTime)journeyData[1], (bool)journeyData[2], (int)journeyData[3], (int)journeyData[4], (double)journeyData[5]);
+        }
+
+        /// <summary>
+        /// Extracts the row data
+        /// </summary>
+        /// <param name="row">The row to extract</param>
+        /// <returns>An array of extracted data</returns>
+        protected object[] ExtractRow(DataRow row)
+        {
+            Destination destination = Destination.Billund;
+            DateTime departureTime = row.Field<DateTime>("DepartureTime");
+            bool isFirstClass = row.Field<bool>("IsFirstClass");
+            int adults = row.Field<int>("Adults"), children = row.Field<int>("Children");
+            double luggageAmount = (double)row.Field<decimal>("LuggageAmount");
+
+            switch(row.Field<string>("Destination"))
+            {
+                case "Copenhagen":
+                    destination = Destination.Copenhagen;
+                    break;
+                case "PalmaDeMallorca":
+                    destination = Destination.PalmaDeMallorca;
+                    break;
+            }
+
+            return new object[6] { destination, departureTime, isFirstClass, adults, children, luggageAmount };
         }
     }
 }
