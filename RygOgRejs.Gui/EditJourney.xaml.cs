a0f416f9c30328331c932e68338320a096d1a6c8
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-29 11:47:14 +0200

    Added EditJourney user control

---
+++ RygOgRejs.Gui/EditJourney.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RygOgRejs.Entities;
+
+namespace RygOgRejs.Gui
+{
+    /// <summary>
+    /// Interaction logic for EditJourney.xaml
+    /// </summary>
+    public partial class EditJourney : UserControl
+    {
+        Journey journey;
+
+        public EditJourney(Journey journey)
+        {
+            InitializeComponent();
+            this.journey = journey;
+        }
+
+        public void Update(Journey newJourney)
+        {
+            journey = newJourney;
+            // Update data
+
+        }
+    }
+}
