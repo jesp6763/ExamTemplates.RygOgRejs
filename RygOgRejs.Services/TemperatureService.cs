4a13f28d4e8b8b356409aa7dd40c4cdf7bf0d93b
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-29 09:08:29 +0200

    Added temperature method. Still work in progress

--- RygOgRejs.Services/TemperatureService.cs
+++ RygOgRejs.Services/TemperatureService.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace RygOgRejs.Services
 {
-    public class TemperatureService
+    public static class TemperatureService
     {
-        public string GetTemperature(string destination)
+        public static string GetTemperature(string destination)
         {
-            return string.Empty;
-            //return $"{destinationsTemp} C";
+            WebClient client = new WebClient();
+            string json = client.DownloadString("http://samples.openweathermap.org/data/2.5/find?q=London&units=metric&appid=b1b15e88fa797225412429c1c50c122a1");
+            dynamic data = JsonConvert.DeserializeObject(json);
+
+            if(data["list"][0]["main"] == null)
+            {
+                return "No temperature was found";
+            }
+            return $"{data["list"][0]["main"].temp} °C";
         }
     }
 }
+
+
+// OpenWeatherMap API Key: 2a5b989adc59b59f198ee28ce3dbeeee
