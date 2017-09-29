a0544cca8b4e57a0365ff89e9aa7f27a2cf1da63
jesp6763 <jesp6763@edu.campusvejle.dk>
2017-09-29 09:57:16 +0200

    Added 2 more tests and changed name of 2 test methods

--- RygOgRejs.Test/PriceDetailsTests.cs
+++ RygOgRejs.Test/PriceDetailsTests.cs
@@ -12,7 +12,7 @@ namespace RygOgRejs.Test
         /// Tests if the calculation without tax is correct
         /// </summary>
         [TestMethod]
-        public void CorrectPriceCalculationWithoutTax()
+        public void CorrectPriceCalculationWithoutTax1()
         {
             // Arrange
             decimal expected = 2784m;
@@ -30,12 +30,42 @@ namespace RygOgRejs.Test
         /// Tests if the calculation with tax is correct
         /// </summary>
         [TestMethod]
-        public void CorrectPriceCalculationWithTax()
+        public void CorrectPriceCalculationWithTax1()
         {
             // Arrange
             decimal expected = 3480m;
             decimal actual;
             PriceDetails priceDetails = new PriceDetails(Destination.Billund, 2, 1, true, 0);
+
+            // Act
+            actual = priceDetails.GetTotalWithTax();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CorrectPriceCalculationWithoutTax2()
+        {
+            // Arrange
+            decimal expected = 10150m;
+            decimal actual;
+            PriceDetails priceDetails = new PriceDetails(Destination.Copenhagen, 4, 0, false, 13);
+
+            // Act
+            actual = priceDetails.GetTotalWithoutTax();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CorrectPriceCalculationWithTax2()
+        {
+            // Arrange
+            decimal expected = 12687.5m;
+            decimal actual;
+            PriceDetails priceDetails = new PriceDetails(Destination.Copenhagen, 4, 0, false, 13);
 
             // Act
             actual = priceDetails.GetTotalWithTax();
